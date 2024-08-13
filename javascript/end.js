7875da8203a64cb10645bc5b846369ff2887994f
Jasper Orenius <ad4018@student.jamk.fi>
2024-08-13 09:18:48 +0300

    Loppuruutu valmis

--- javascript/end.js
+++ javascript/end.js
@@ -1,2 +1,5 @@
 const score = localStorage.getItem('score');
 document.getElementById('score-display').textContent = `Pisteet: ${score}/15`;
+function returnToMenu() {
+    window.location.href = "../index.html";
+}
