d008121ee0380164e3e7f71a83223d7e95df1bd8
Jasper-Orenius <167324971+Jasper-Orenius@users.noreply.github.com>
2024-09-23 17:32:33 +0300

    Tiedostojen nimistä ääkköset ja välit pois

--- javascript/peli.js
+++ javascript/peli.js
@@ -28,27 +28,27 @@ falseButton.addEventListener('click', function() {
 const info = [
     {
         text: "Tien ylittäminen: Kun haluat ylittää tien, käytä aina suojatietä, jos sellainen on lähellä. Ennen kuin astut tielle, katso molempiin suuntiin ja varmista, että autot ovat pysähtyneet ja on turvallista ylittää. Muista: katso ensin vasemmalle, sitten oikealle ja vielä kerran vasemmalle.",
-        imageUrl: "../images/tien_säännöt-suojatie1.png"
+        imageUrl: "../images/tien_saannot-suojatie1.png"
     },
     {
         text: "Pyöräily: Jos pyöräilet, muista käyttää aina kypärää. Se suojaa päätäsi, jos kaadut. Pyöräillessäsi kulje pyörätietä tai tien oikeaa reunaa. Muista myös näyttää kädelläsi suuntamerkkiä, kun käännyt. Näin muut tietävät, minne olet menossa.",
-        imageUrl: "../images/tien_säännöt-kypärän_käyttö1.2.png"
+        imageUrl: "../images/tien_saannot-kyparan_kaytto1.2.png"
     },
     {
         text: "Kävely: Kun kävelet, kulje jalkakäytävällä tai tien vasemmassa reunassa, jos jalkakäytävää ei ole. Tämä auttaa näkemään vastaantulevat autot paremmin. Ole aina tarkkana ja vältä leikkimistä tiellä.",
-        imageUrl: "../images/tien_säännöt-pyörätie1.png"
+        imageUrl: "../images/tien_saannot-pyoratie1.png"
     },
     {
         text: "Liikennevalot: Liikennevalot kertovat, milloin voit kävellä ja milloin täytyy odottaa. Kun valo on vihreä, voit ylittää tien, mutta silti on hyvä katsoa, että autot todella pysähtyvät. Jos valo on punainen, odota, vaikka muita ihmisiä menisi tien yli.",
-        imageUrl: "../images/tien_säännöt-liikennevalot1.png"
+        imageUrl: "../images/tien_saannot-liikennevalot1.png"
     },
     {
         text: "Autossa matkustaminen: Kun matkustat autossa, käytä aina turvavyötä. Se pitää sinut turvassa, jos auto joutuu onnettomuuteen. Pienempien lasten on hyvä käyttää turvaistuinta, joka sopii heidän kokoonsa.",
-        imageUrl: "../images/tien_säännöt-turvavyö1.png"
+        imageUrl: "../images/tien_saannot-turvavyo1.png"
     },
     {
         text: "Kuuntele aikuisia: Muista kuunnella vanhempia ja opettajia, kun he neuvovat liikenteessä. Heidän ohjeensa auttavat sinua pysymään turvassa.",
-        imageUrl: "../images/tien_säännöt-peli1.png"
+        imageUrl: "../images/tien_saannot-peli1.png"
     }
 ]
 
@@ -56,77 +56,77 @@ const questions = [
     {
         question: "Valojen käyttö polkupyörässä on tärkeää pimeällä",
         answer: true,
-        imageUrl: "../images/tien_säännöt-valot1.png"
+        imageUrl: "../images/tien_saannot-valot1.png"
     },
     {
         question: "Polkupyörässä ei kannata käyttää heijastimia",
         answer: false,
-        imageUrl: "../images/tien_säännöt-valot&heijastimet1.png"
+        imageUrl: "../images/tien_saannot-valot&heijastimet1.png"
     },
     {
         question: "Turvavyötä pitäisi aina käyttää autossa",
         answer: true,
-        imageUrl: "../images/tien_säännöt-turvavyö1.png"
+        imageUrl: "../images/tien_saannot-turvavyo1.png"
     },
     {
         question: "Tasoristeystä lähestyessä pitää olla aina erityisen varovainen",
         answer: true,
-        imageUrl: "../images/tien_säännöt-tasoristeys1.png"
+        imageUrl: "../images/tien_saannot-tasoristeys1.png"
     },
     {
         question: "Suojatietä ylittäessä pitää aina tarkkailla molempiin suuntiin",
         answer: true,
-        imageUrl: "../images/tien_säännöt-tarkkaavaisuus1.png"
+        imageUrl: "../images/tien_saannot-tarkkaavaisuus1.png"
     },
     {
         question: "Suuntamerkkiä ei tarvitse käyttää kaistaa vaihtaessa tai kääntyessä",
         answer: false,
-        imageUrl: "../images/tien_säännöt-suuntamarkki1.png"
+        imageUrl: "../images/tien_saannot-suuntamarkki1.png"
     },
     {
         question: "Suojatietä pitää aina käyttää kun ylittää tien",
         answer: true,
-        imageUrl: "../images/tien_säännöt-suojatie1.png"
+        imageUrl: "../images/tien_saannot-suojatie1.png"
     },
     {
         question: "Stop merkin kohdalle ei tarvitse pysähtyä",
         answer: false,
-        imageUrl: "../images/tien_säännöt-stop1.png"
+        imageUrl: "../images/tien_saannot-stop1.png"
     },
     {
         question: "Kävelijät voi kävellä pyörätiellä, jos haluaa",
         answer: false,
-        imageUrl: "../images/tien_säännöt-pyörätie1.png"
+        imageUrl: "../images/tien_saannot-pyoratie1.png"
     },
     {
         question: "Pyörä kannattaa aina lukita, kun jättää sen parkkiin",
         answer: true,
-        imageUrl: "../images/tien_säännöt-pyöräparkki1.png"
+        imageUrl: "../images/tien_saannot-pyoraparkki1.png"
     },
     {
         question: "Mäkeä laskiessa kannattaa katsoa, että ketään ei ole edessä",
         answer: true,
-        imageUrl: "../images/tien_säännöt-mäenlasku1.png"
+        imageUrl: "../images/tien_saannot-maenlasku1.png"
     },
     {
         question: "Liukkaalla kelillä kannattaa liikkua varovaisesti",
         answer: true,
-        imageUrl: "../images/tien_säännöt-liukkaus1.png"
+        imageUrl: "../images/tien_saannot-liukkaus1.png"
     },
     {
         question: "Tietä ylittäessä kannattaa katsoa suoraan alaspäin",
         answer: false,
-        imageUrl: "../images/tien_säännöt-katso1.png"
+        imageUrl: "../images/tien_saannot-katso1.png"
     },
     {
         question: "Heijastimen käyttö on tärkeää pimeällä",
         answer: true,
-        imageUrl: "../images/tien_säännöt-heijastin1.png"
+        imageUrl: "../images/tien_saannot-heijastin1.png"
     },
     {
         question: "Aikuisten ja opettajien neuvoja ei kannata kuunnella liikenteessä",
         answer: false,
-        imageUrl: "../images/tien_säännöt-bussipysäkki1.png"
+        imageUrl: "../images/tien_saannot-bussipysakki1.png"
     },
 ];
 
@@ -232,8 +232,8 @@ function updateButtonStyling() {
 function resetButtonStyles() {
     trueButton.classList.remove('correct', 'incorrect', 'correct_green', 'correct_red', 'incorrect_green', 'incorrect_red');
     falseButton.classList.remove('correct', 'incorrect', 'correct_green', 'correct_red', 'incorrect_green', 'incorrect_red');
-    trueButton.style.backgroundImage = "url('../images/Oikein\ Ж.png')";
-    falseButton.style.backgroundImage = "url('../images/Väärin\ Ж.png')";
+    trueButton.style.backgroundImage = "url('../images/Oikein.png')";
+    falseButton.style.backgroundImage = "url('../images/Vaarin.png')";
 }
 
 toggleNavigationButtons();
